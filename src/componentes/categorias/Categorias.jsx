8b038155ebed770b7d7b40c4abee7b7fb9b7162d
Laiza <l.aiza.saraiva@gmail.com>
2025-09-13 19:57:04 -0300

    refactor: Refatora a estrutura alterando os arrays de dados para um arquivo separado de mock.

--- src/componentes/categorias/Categorias.jsx
+++ src/componentes/categorias/Categorias.jsx
@@ -1,14 +1,7 @@
+import categorias from '../../services/Mocks/categorias';
 import './categorias.css';
 
 function Categorias({ categoria, setCategoria }) {
-  const categorias = [
-    { id: 'hamburgers', nome: 'Burger', icone: './img/iconeBurger.png', target: 'hamburgers' },
-    { id: 'pizzas', nome: 'Pizza', icone: './img/iconePizza.png', target: 'pizzas' },
-    { id: 'sorvetes', nome: 'Ice Cream', icone: './img/iconeIceCream.png', target: 'sorvetes' },
-    { id: 'snacks', nome: 'Snack', icone: './img/iconeSnacks.png', target: 'snacks' },
-    { id: 'drinks', nome: 'Drink', icone: './img/iconeDrinks.png', target: 'drinks' },
-  ];
-
   return (
     <section className="categorias">
       <h2>Category</h2>
---
+++ src/services/Mocks/categorias.js
@@ -0,0 +1,9 @@
+const categorias = [
+  { id: 'hamburgers', nome: 'Burger', icone: './img/iconeBurger.png', target: 'hamburgers' },
+  { id: 'pizzas', nome: 'Pizza', icone: './img/iconePizza.png', target: 'pizzas' },
+  { id: 'sorvetes', nome: 'Ice Cream', icone: './img/iconeIceCream.png', target: 'sorvetes' },
+  { id: 'snacks', nome: 'Snack', icone: './img/iconeSnacks.png', target: 'snacks' },
+  { id: 'drinks', nome: 'Drink', icone: './img/iconeDrinks.png', target: 'drinks' },
+];
+
+export default categorias;
